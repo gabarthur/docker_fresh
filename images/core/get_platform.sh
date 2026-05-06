1be1ed9b9e53671ae1df43ebeefdf7086cb02a08
gabarth <gabarth@live.com>
2026-05-06 10:58:50 +0300

    fix

--- images/core/get_platform.sh
+++ images/core/get_platform.sh
@@ -2,5 +2,5 @@ cd /out_files
 
 unzip server*.zip -d /main_dir/distr
 
-mkdir /main_dir/distr/license
+mkdir /main_dir/distr/license_tools
 tar -xvf *license_tools*.tar.gz -C /main_dir/distr/license_tools
