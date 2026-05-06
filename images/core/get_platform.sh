7559701b3b67daca25d3e47072e42330cee445b5
gabarth <gabarth@live.com>
2026-05-06 10:56:26 +0300

    добавил установку license-tools

--- images/core/get_platform.sh
+++ images/core/get_platform.sh
@@ -3,4 +3,4 @@ cd /out_files
 unzip server*.zip -d /main_dir/distr
 
 mkdir /main_dir/distr/license
-tar -xvf *license_tools*.tar.gz -C /main_dir/distr/license
+tar -xvf *license_tools*.tar.gz -C /main_dir/distr/license_tools
