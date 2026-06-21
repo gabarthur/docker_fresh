1d62d3a383719faf940cf7a3ee1ba3b530d4b239
Габдулхаков Артур <agab@1c.ru>
2026-06-21 09:26:50 +0000

    Заменил entrypoint

--- images/core/entrypoint.sh
+++ images/core/entrypoint.sh
@@ -1,62 +1,55 @@
 #!/bin/bash
 
-set -e
+set -em
 
-platform_path=$(find / -name "1cv8c" | sed 's/1cv8c//g' | sed 's/\.\/opt/\/opt/g')
+export DISPLAY=:99
+/usr/bin/Xvfb ${DISPLAY} -screen 0 1680x1050x24 &
+/usr/bin/x11vnc -forever -noxfixes &
+sleep 5
+startxfce4 &
+status=$?
+if [ $status -ne 0 ]; then
+    echo "Failed to start Xvfb: $status"
+    exit $status
+fi
 
-if [ "$1" = 'srv' ]
+if [ "$CONTAINER_NAME" = 'srv' ]
 then
-    chown -R usr1cv8:grp1cv8 ${COREDATA} ${CORELOGS}
-    exec gosu usr1cv8 ${platform_path}ragent -debug -http /d ${COREDATA}
-elif [ "$1" = 'srv+cli' ]
-then
-    ulimit -c unlimited
-    chown -R usr1cv8:grp1cv8 ${COREDATA} ${CORELOGS}
-    exec gosu usr1cv8 ${platform_path}ragent -debug -http /d ${COREDATA} &
-    status=$?
-    if [ $status -ne 0 ]; then
-        echo "Failed to start ragent: $status"
-        exit $status
+    echo "Starting Executor in $CONTAINER_NAME"
+    chown -R usr1cv8:grp1cv8 ${COREDATA} ${CORELOGS} ${EXECUTOR_DATA} ${EXECUTOR_HOME} ${LIC_DATA} ${VOLUMES_PATH} ${MNT_PATH} ${DUMP_PATH}
+    if [ ! -f "${EXECUTOR_DATA}/Linux_Server_Start.sh" ]
+    then
+        exec gosu usr1cv8 ragent -debug -http -d ${COREDATA}
+    else
+        exec gosu usr1cv8 ragent -debug -http -d ${COREDATA} &
+        while [ ! -f "${EXECUTOR_DATA}/data/connection_settings.json" ]
+        do
+            sleep 1
+        done
+        exec gosu usr1cv8 ${EXECUTOR_DATA}/Linux_Server_Start.sh &
+        fg %1
     fi
-    rm -rf /tmp/.X99-lock
-    exec /usr/bin/Xvfb :99 -screen 0 1680x1050x24 -shmem &
-    sleep 5 &&
-    exec /usr/bin/x11vnc -display :99 -forever &
-    status=$?
-    if [ $status -ne 0 ]; then
-        echo "Failed to start Xvfb: $status"
-        exit $status
+elif [ "$CONTAINER_NAME" = 'ras' ]
+then
+    echo "Working in $CONTAINER_NAME"
+    chown -R usr1cv8:grp1cv8 ${CORELOGS}
+    exec gosu usr1cv8 "$@"
+elif [ "$CONTAINER_NAME" = 'web' ]
+then
+    echo "Starting Executor in $CONTAINER_NAME"
+    chown -R www-data:www-data ${CORELOGS} ${FRESH} ${EXECUTOR_DATA} ${EXECUTOR_HOME}
+    if [ ! -f "${EXECUTOR_DATA}/Linux_Server_Start.sh" ]
+    then
+        exec apachectl -DFOREGROUND
+    else
+        exec apachectl -DFOREGROUND &
+        while [ ! -f "${EXECUTOR_DATA}/data/connection_settings.json" ]
+        do
+            sleep 1
+        done
+        exec gosu www-data ${EXECUTOR_DATA}/Linux_Server_Start.sh &
+        fg %1
     fi
-    while sleep 60; do
-        ps aux | grep [r]agent
-        RAGENT_STATUS=$?
-        ps aux | grep [Xvfb]
-        XVFB_STATUS=$?
-        if [ $RAGENT_STATUS -ne 0 -o $XVFB_STATUS -ne 0 ]; then
-            echo "One of the processes has already exited."
-            exit 1
-        fi
-    done    
-elif [ "$1" = 'ras' ]
-then
-    chown -R usr1cv8:grp1cv8 ${CORELOGS}
-    exec gosu usr1cv8 ${platform_path}ras cluster
-elif [ "$1" = 'cli' ]
-then
-    chown -R usr1cv8:grp1cv8 ${CORELOGS}
-    exec /usr/bin/Xvfb :99 -screen 0 1680x1050x24 -shmem &
-    exec /usr/bin/x11vnc -display :99 -forever 
-elif [ "$1" = 'web' ]
-then
-    chown -R usr1cv8:grp1cv8 ${CORELOGS}
-    rm -rf /run/httpd/* /tmp/httpd*
-    unset HOME
-    exec httpd -DFOREGROUND
-elif [ "$1" = 'agent' ]
-then
-    chown -R usr1cv8:grp1cv8 ${COREDATA} ${CORELOGS} ${AGENTBASEDIR}
-    exec /usr/bin/Xvfb :99 -screen 0 1680x1050x24 -shmem &
-    exec ${platform_path}1cv8 DESIGNER /AgentMode /IBConnectionString "${INFOBASECONNECTIONSTRING}" /AgentBaseDir "${AGENTBASEDIR}" /AgentSSHHostKey "/id_rsa.key" /Visible /AgentListenAddress 0.0.0.0
 fi
 
 exec "$@"
