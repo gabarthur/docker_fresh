ba88b65a89ba28cde6f323f967d68c56a885fa6f
Габдулхаков Артур <agab@1c.ru>
2026-06-23 11:22:59 +0000

    fix entrypoint db

--- images/db/entrypoint.sh
+++ images/db/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
- 
+
 set -e
- 
+
 if [ "$1" = 'postgres' ]; then
     chown -R postgres:postgres "$PGDATA" /var/run/postgresql
     if [ ! -s "$PGDATA/PG_VERSION" ]; then
@@ -17,14 +17,14 @@ if [ "$1" = 'postgres' ]; then
         echo "log_min_error_statement = error" >> "$PGDATA/postgresql.conf"
         echo "log_timezone = 'Europe/Moscow'" >> "$PGDATA/postgresql.conf"
         echo "log_line_prefix = '%t [%p]: [%l],user=%u,db=%d,app=%a,client=%h '" >> "$PGDATA/postgresql.conf"
-        echo "log_connections = authorization" >> "$PGDATA/postgresql.conf"
+        echo "log_connections = on" >> "$PGDATA/postgresql.conf"
         echo "log_disconnections = on" >> "$PGDATA/postgresql.conf"
         sed -i 's/max_connections = 100/max_connections = 1000/' "$PGDATA/postgresql.conf"
         echo "host    all             all             0.0.0.0/0               trust" >> "$PGDATA/pg_hba.conf"
     fi
- 
+
     exec gosu postgres postgres -c jit=off &
     tail -F "$PGDATA/log/postgresql.log"
 fi
- 
+
 exec "$@"
